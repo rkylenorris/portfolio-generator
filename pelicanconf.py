0b249cb7de61a520d7812a20c2c2ef971cd416a8
R. Kyle Norris <r.kyle.norris@gmail.com>
2026-02-10 23:04:27 -0500

    updated project config info

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,21 +1,22 @@
 AUTHOR = 'R. Kyle Norris'
 SITENAME = 'Portfolio: R. Kyle Norris'
-SITEURL = ""
+SITEURL = '.'
+RELATIVE_URLS = True
 
 PATH = "content"
 
 TIMEZONE = 'America/New_York'
 
-DEFAULT_LANG = 'English'
+DEFAULT_LANG = 'en'
 
 # Theme settings
 THEME = 'theme'  # We will move your CSS/HTML here
 
 # Static paths
-STATIC_PATHS = ['images', 'extra/favicon.ico']
-EXTRA_PATH_METADATA = {
-    'extra/favicon.ico': {'path': 'favicon.ico'}
-}
+# STATIC_PATHS = ['images', 'extra/favicon.ico']
+# EXTRA_PATH_METADATA = {
+#     'extra/favicon.ico': {'path': 'favicon.ico'}
+# }
 
 # Display pages in menu
 DISPLAY_PAGES_ON_MENU = True
